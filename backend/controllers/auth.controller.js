5d4bc460664fa8bf2a9e31f63c54a6add304fd0a
Trung Vu <trung.vd225161@sis.hust.edu.vn>
2025-02-18 01:03:18 +0700

    project setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+export const signup = async (req, res) => {
+    res.json({
+        message: "Signup works!"
+    });
+};
+
+export const login = async (req, res) => {
+    res.json({
+        message: "Login works!"
+    });
+};
+
+export const logout = async (req, res) => {
+    res.json({
+        message: "Logout works!"
+    });
+};
