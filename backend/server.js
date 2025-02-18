77294299d4f9a47cf305769cd855b005a2da8329
Trung Vu <trung.vd225161@sis.hust.edu.vn>
2025-02-19 00:49:00 +0700

    auth controllers added

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
 import authRoutes from './routes/auth.routes.js';
 import connectMongoDB from './db/connetMongoDB.js';
@@ -9,11 +10,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json()); // to parse req.body
+app.use(express.urlencoded({ extended: true })); // to parse form data (urlencoded)
+
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 
-console.log(process.env.MONGODB_URI);
-
-app.listen(8000, () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     connectMongoDB();
 });
