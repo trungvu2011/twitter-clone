20cc5594fc83b77ee6f6c2b0a147d2900280d75e
Trung Vu <trung.vd225161@sis.hust.edu.vn>
2025-02-20 14:47:33 +0700

    user routes and controllers added

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -10,6 +10,11 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
     password: {
         type: String,
         required: true,
