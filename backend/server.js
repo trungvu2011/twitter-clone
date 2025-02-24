c237fd8c7f8abe9e0780f38b31a7262809b487be
Trung Vu <trung.vd225161@sis.hust.edu.vn>
2025-02-24 21:13:36 +0700

    payload limit updated in server.js

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,8 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json()); // to parse req.body
+app.use(express.json({ limit: "5mb" })); // to parse req.body
+// limit sholdn't be too high, because it can be used for DDoS attacks
 app.use(express.urlencoded({ extended: true })); // to parse form data (urlencoded)
 app.use(cors({
     origin: "http://localhost:3000", // Frontend URL
