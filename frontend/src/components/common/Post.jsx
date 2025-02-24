d9a5f6f808dd8c05c091dfc9fc91c6f33ac7275f
Trung Vu <trung.vd225161@sis.hust.edu.vn>
2025-02-25 00:20:19 +0700

    comment on post mutation added

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -8,11 +8,19 @@ import { Link } from "react-router-dom";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import LoadingSpinner from "./LoadingSpinner";
+import { formatPostDate } from "../../utils/date/index";
 
 const Post = ({ post }) => {
     const [comment, setComment] = useState("");
     const { data: authUser } = useQuery({ queryKey: ["authUser"] });
     const queryClient = useQueryClient();
+
+    const postOwner = post.user;
+    const isLiked = post.likes.includes(authUser._id);
+
+    const isMyPost = authUser._id == post.user._id;
+
+    const formattedDate = formatPostDate(post.createdAt);
 
     const { mutate: deletePost, isPending: isDeleting } = useMutation({
         mutationFn: async () => {
@@ -77,14 +85,51 @@ const Post = ({ post }) => {
         }
     });
 
-    const postOwner = post.user;
-    const isLiked = post.likes.includes(authUser._id);
+    const { mutate: commentPost, isPending: isCommenting } = useMutation({
+        mutationFn: async () => {
+            try {
+                const res = await fetch(`/api/posts/comment/${post._id}`, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({ text: comment })
+                });
 
-    const isMyPost = authUser._id == post.user._id;
+                const data = await res.json();
 
-    const formattedDate = "1h";
+                if (!res.ok) {
+                    throw new Error(data.error || "Something went wrong");
+                }
 
-    const isCommenting = true;
+                return data;
+            } catch (error) {
+                throw new Error(error);
+            }
+        },
+        onSuccess: () => {
+            toast.success("Comment posted successfully");
+            setComment("");
+            // invalidate the query to refetch the data
+            // queryClient.invalidateQueries({ queryKey: ["posts"] });
+            queryClient.setQueryData(["posts"], (oldData) => {
+                return oldData.map(p => {
+                    if (p._id === post._id) {
+                        return {
+                            ...p,
+                            comments: [...p.comments, { text: comment, user: authUser }]
+                        }
+                    }
+                    return p;
+                });
+            });
+            // close the modal
+            document.getElementById("comments_modal" + post._id).close();
+        },
+        onError: (error) => {
+            toast.error(error.message);
+        }
+    });
 
     const handleDeletePost = () => {
         deletePost();
@@ -92,6 +137,8 @@ const Post = ({ post }) => {
 
     const handlePostComment = (e) => {
         e.preventDefault();
+        if (isCommenting) return;
+        commentPost();
     };
 
     const handleLikePost = () => {
