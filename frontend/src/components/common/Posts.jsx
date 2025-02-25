3ac14b03860c8f7702afc2f45fdb4681f687f2ab
Trung Vu <trung.vd225161@sis.hust.edu.vn>
2025-02-25 22:24:56 +0700

    Profile page posts and user into fetched

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -3,20 +3,24 @@ import PostSkeleton from "../skeletons/PostSkeleton";
 import { useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
 
-const Posts = ({ feedType }) => {
-
+const Posts = ({ feedType, username, userId }) => {
     const getPostEndpoint = () => {
         switch (feedType) {
             case "forYou":
                 return "/api/posts/all";
             case "following":
                 return "/api/posts/following";
+            case "posts":
+                return `/api/posts/user/${username}`;
+            case "likes":
+                return `/api/posts/likes/${userId}`;
             default:
                 return "/api/posts/all";
         }
     }
 
     const POST_ENDPOINT = getPostEndpoint();
+
     const { data: posts, isLoading, refetch, isRefetching } = useQuery({
         queryKey: ["posts"],
         queryFn: async () => {
@@ -37,7 +41,7 @@ const Posts = ({ feedType }) => {
 
     useEffect(() => {
         refetch();
-    }, [feedType, refetch]);
+    }, [feedType, refetch, username]);
 
     return (
         <>
